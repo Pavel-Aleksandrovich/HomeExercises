20f37b0e54e1a288d53d6e7bcd6f75700499f180
Pavel Mishanin <pavelmishanin@mail.ru>
2022-04-15 15:16:27 +0700

    Initial Commit

---
+++ HomeExercises/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeExercises
+//
+//  Created by pavel mishanin on 15.04.2022.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    private let button = UIButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        title = "ViewController"
+    }
+}
